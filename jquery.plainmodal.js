7bd9a0190b76e4813ff7a29eca5031b3f1e434bd
Evan Sharp <ego@evansharp.ca>
2014-04-21 18:15:21 -0700

    Fixed onClose option

--- jquery.plainmodal.js
+++ jquery.plainmodal.js
@@ -21,7 +21,7 @@ function init(jq, options) {
         duration:       200,
         effect:         {open: $.fn.fadeIn, close: $.fn.fadeOut},
         overlay:        {color: '#888', opacity: 0.6, zIndex: 9000},
-        onClose:        function() {},
+        onClose:        function(){},
         closeClass:     'plainmodal-close'
         //offset
       }, options);
@@ -142,10 +142,8 @@ function modalClose(jq) { // jq: target/event
       jqOpened = 0;
     }
   }
+  opt.onClose.call(this);
   if (isEvent) { jq.preventDefault(); return false; }
-  if(typeof opt.onClose == 'function'){
-      opt.onClose.call(this);
-  }
   return jq;
 }
 
@@ -159,7 +157,7 @@ $.fn.plainModal = function(action, options) {
   return (
     action === 'open' ?   modalOpen(this, options) :
     action === 'close' ?  modalClose(this) :
-    init(this, action)); // options.
+    									init(this, action)); // options.
 };
 
 })(jQuery);
